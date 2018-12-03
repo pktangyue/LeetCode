3ae5f8a94071cfd3f4b6c9ee50368401768befc1
pktangyue <tangyue1004@gmail.com>
2018-12-03 18:19:13 +0800

    tab -> 4indent

--- go/main_test.go
+++ go/main_test.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+    "github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestTwoSum(t *testing.T) {
-	assert.Equal(t,
-		[]int{0, 1},
-		twoSum([]int{2, 7, 11, 15}, 9),
-	)
+    assert.Equal(t,
+        []int{0, 1},
+        twoSum([]int{2, 7, 11, 15}, 9),
+    )
 }
 
 func TestAddTwoNumbers(t *testing.T) {
-	assert.Equal(t,
-		&ListNode{7, &ListNode{0, &ListNode{8, nil}}},
-		addTwoNumbers(
-			&ListNode{2, &ListNode{4, &ListNode{3, nil}}},
-			&ListNode{5, &ListNode{6, &ListNode{4, nil}}},
-		),
-	)
-	assert.Equal(t,
-		&ListNode{7, &ListNode{0, &ListNode{0, &ListNode{1, nil}}}},
-		addTwoNumbers(
-			&ListNode{2, &ListNode{4, &ListNode{5, nil}}},
-			&ListNode{5, &ListNode{6, &ListNode{4, nil}}},
-		),
-	)
-	assert.Equal(t,
-		&ListNode{7, &ListNode{0, &ListNode{8, &ListNode{1, nil}}}},
-		addTwoNumbers(
-			&ListNode{2, &ListNode{4, &ListNode{3, &ListNode{1, nil}}}},
-			&ListNode{5, &ListNode{6, &ListNode{4, nil}}},
-		),
-	)
-	assert.Equal(t,
-		&ListNode{7, &ListNode{0, &ListNode{8, &ListNode{1, nil}}}},
-		addTwoNumbers(
-			&ListNode{2, &ListNode{4, &ListNode{3, nil}}},
-			&ListNode{5, &ListNode{6, &ListNode{4, &ListNode{1, nil}}}},
-		),
-	)
+    assert.Equal(t,
+        &ListNode{7, &ListNode{0, &ListNode{8, nil}}},
+        addTwoNumbers(
+            &ListNode{2, &ListNode{4, &ListNode{3, nil}}},
+            &ListNode{5, &ListNode{6, &ListNode{4, nil}}},
+        ),
+    )
+    assert.Equal(t,
+        &ListNode{7, &ListNode{0, &ListNode{0, &ListNode{1, nil}}}},
+        addTwoNumbers(
+            &ListNode{2, &ListNode{4, &ListNode{5, nil}}},
+            &ListNode{5, &ListNode{6, &ListNode{4, nil}}},
+        ),
+    )
+    assert.Equal(t,
+        &ListNode{7, &ListNode{0, &ListNode{8, &ListNode{1, nil}}}},
+        addTwoNumbers(
+            &ListNode{2, &ListNode{4, &ListNode{3, &ListNode{1, nil}}}},
+            &ListNode{5, &ListNode{6, &ListNode{4, nil}}},
+        ),
+    )
+    assert.Equal(t,
+        &ListNode{7, &ListNode{0, &ListNode{8, &ListNode{1, nil}}}},
+        addTwoNumbers(
+            &ListNode{2, &ListNode{4, &ListNode{3, nil}}},
+            &ListNode{5, &ListNode{6, &ListNode{4, &ListNode{1, nil}}}},
+        ),
+    )
 }
