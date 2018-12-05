577191e85ded9919aa1c6aab2562e8eba9217466
pktangyue <tangyue1004@gmail.com>
2018-12-05 17:27:41 +0800

    262_TripsAndUsers

---
+++ go/262_TripsAndUsers.go
@@ -0,0 +1,7 @@
+package main
+
+//select Request_at as Day,
+//      ROUND(sum(Status = 'cancelled_by_driver' or Status = 'cancelled_by_client') / count(t.id), 2)  as 'Cancellation Rate'
+//from Trips t join Users u on t.Client_Id = u.Users_Id and u.Banned = 'No'
+//where Request_at >= '2013-10-01' and Request_at <= '2013-10-03'
+//group by Request_at;
